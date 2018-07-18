7b5db9b927ff2d8aabc5d5568529b2f98bf8e41b
unknown <namju615@hanmail.net>
2018-07-18 15:43:52 +0900

    JSX

--- njkim/01/src/App.js
+++ njkim/01/src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 class App extends Component {
   render() {
+    const style = {
+      backgroundColor : 'black',
+      padding : '16px',
+      color : 'aqua',
+      fontSize : '36px'
+    };
+
+    const value = 1;
+    const name = "React"
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      (() => {
+        if(value === 1) return (<div class="App">Hi</div>);
+        if(value === 2) return (<div style={style}>{name}</div>);
+      })()
+    
     );
   }
 }
